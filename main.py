39e3b8c7d5a4814b9622b2d84d02708d49e8e0e9
Victor Candeira <vscandeira@gmail.com>
2025-07-13 16:34:41 -0300

    Ajuste no main para admitir arquivos na chamada do programa. Restante foi comentado para possibilitar correto funcionamento.

--- main.py
+++ main.py
@@ -3,13 +3,32 @@
 #Instancia o pseudo-SO.
 #Gera a saída do terminal.
 
+import sys
 from utils.parser import parse_processos, parse_arquivos
 from core.sistema_operacional import SistemaOperacional
 
 if __name__ == "__main__":
-    processos = parse_processos("processos.txt")
-    operacoes_arquivos = parse_arquivos("arquivos.txt")
+    if len(sys.argv) != 3:
+        print("Uso: python main.py <processos.txt> <arquivos.txt>")
+        sys.exit(1)
 
-    so = SistemaOperacional(processos, operacoes_arquivos)
-    so.executar()
+    procs_param = sys.argv[1]
+    file_ops_param = sys.argv[2]
+
+    # Verificação inicial, será retirado em release final
+    print("Processos:", procs_param)
+    print("Operações de Arquivos:", file_ops_param)
+
+    # Parseia os processos e arquivos
+    procs = parse_processos(procs_param)
+    file_ops = parse_arquivos(file_ops_param)
+
+    # Verificação inicial, será retirado em release final
+    print("Processos:", procs)
+    print("Operações de Arquivos:", file_ops)
+
+    # Instancia o despachante
+    # Despachante(processos, operacoes_arquivos).executar()
+    #so = SistemaOperacional(processos, operacoes_arquivos)
+    #so.executar()
 
