9b14bacc38bf029683b4b42a1b3d815fbb8a0556
maldisco <filipe.sf04@gmail.com>
2025-07-16 11:15:45 -0300

    add tests

--- tests/test_memoria.py
+++ tests/test_memoria.py
@@ -1,5 +1,6 @@
 import pytest
-from core.memoria import GerenciadorMemoria
+from core.memoria import GerenciadorMemoria, MemoryOverflowError
+
 
 class Processo:
     def __init__(self, pid, prioridade, blocos_mem):
@@ -8,9 +9,11 @@ class Processo:
         self.blocos_mem = blocos_mem
         self.offset = None
 
+
 @pytest.fixture
 def gerenciador():
     return GerenciadorMemoria()
+
 
 def test_alocar_tempo_real_com_sucesso(gerenciador):
     p = Processo(pid=1, prioridade=0, blocos_mem=10)
@@ -19,6 +22,7 @@ def test_alocar_tempo_real_com_sucesso(gerenciador):
     assert p.offset is not None
     # deve estar na região TR
     assert 0 <= p.offset <= gerenciador.BLOCO_FIM_TR
+
 
 def test_alocar_tempo_real_sem_espaco_no_tr_mas_com_espaco_usuario(gerenciador):
     # preenche região TR toda
@@ -31,12 +35,14 @@ def test_alocar_tempo_real_sem_espaco_no_tr_mas_com_espaco_usuario(gerenciador):
     # deve estar na região usuário, pois TR está cheia
     assert gerenciador.BLOCO_INICIO_USUARIO <= p.offset <= gerenciador.BLOCO_FIM_USUARIO
 
+
 def test_alocar_usuario(gerenciador):
     p = Processo(pid=3, prioridade=1, blocos_mem=20)
     sucesso = gerenciador.alocar(p)
     assert sucesso
     # deve estar na região usuário
     assert gerenciador.BLOCO_INICIO_USUARIO <= p.offset <= gerenciador.BLOCO_FIM_USUARIO
+
 
 def test_alocar_falha_por_espaco(gerenciador):
     # preenche toda região usuário
@@ -48,6 +54,7 @@ def test_alocar_falha_por_espaco(gerenciador):
     assert not sucesso
     assert p.offset is None
 
+
 def test_liberar_memoria(gerenciador):
     p = Processo(pid=5, prioridade=1, blocos_mem=5)
     gerenciador.alocar(p)
@@ -55,3 +62,21 @@ def test_liberar_memoria(gerenciador):
     gerenciador.liberar(p)
     assert all(block != p.pid for block in gerenciador.memoria)
     assert p.offset is None
+
+
+def test_alocar_memoria_invalida_tr(gerenciador):
+    p = Processo(pid=5, prioridade=0, blocos_mem=70)
+    try:
+        gerenciador.alocar(p)
+        assert False
+    except MemoryOverflowError as e:
+        assert str(e) == "Processo de tempo real não tem blocos suficientes para alocação."
+
+
+def test_alocar_memoria_invalida_user(gerenciador):
+    p = Processo(pid=6, prioridade=1, blocos_mem=1000)
+    try:
+        gerenciador.alocar(p)
+        assert False
+    except MemoryOverflowError as e:
+        assert str(e) == "Processo de usuário não tem blocos suficientes para alocação."
